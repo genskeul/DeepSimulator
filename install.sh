a2cd813b2ba1b87477250f095f8ff2a074aa0165
genskeul <uli_genske@gmx.de>
2019-11-29 13:18:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
+source $CONDA_PREFIX/etc/profile.d/conda.sh
 
 #-> 1. install tensorflow_cdpm
 conda remove --name tensorflow_cdpm --all -y
 conda create --name tensorflow_cdpm python=2.7 -y
-source activate tensorflow_cdpm
-pip install tensorflow==1.2.1
-pip install tflearn==0.3.2
-pip install tqdm==4.19.4
-pip install scipy==0.18.1
-pip install h5py==2.7.1
-pip install numpy==1.13.1
-pip install scikit-learn==0.20.3
-pip install biopython==1.74
-source deactivate
+conda activate tensorflow_cdpm
+conda install tensorflow==1.2.1
+conda install tflearn==0.3.2
+conda install tqdm==4.19.4
+conda install scipy==0.18.1
+conda install h5py==2.7.1
+conda install numpy==1.13.1
+conda install scikit-learn==0.20.3
+conda install biopython==1.74
+conda deactivate
 
 #-> 2. install basecaller
 #--| 2.1 install albacore_2.3.1
